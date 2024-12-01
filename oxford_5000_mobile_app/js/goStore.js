5380366104ab390b376fbc9962bf83c92faf6dd2
BekirGeris <48486272+BekirGeris@users.noreply.github.com>
2024-12-01 22:47:37 +0300

    fix

--- oxford_5000_mobile_app/js/goStore.js
+++ oxford_5000_mobile_app/js/goStore.js
@@ -1,18 +1,20 @@
-document.getElementById("download-app1").addEventListener("click", function (event) {
-    event.preventDefault(); // Varsayılan bağlantı davranışını engeller
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("download-app1").addEventListener("click", function (event) {
+        event.preventDefault(); // Varsayılan bağlantı davranışını engelle
 
-    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+        var userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
-    // iPhone kontrolü
-    if (/iPhone/i.test(userAgent)) {
-        window.location.href = "https://apps.apple.com/app/id6479473338"; // iPhone için yönlendirme
-    }
-    // Android kontrolü
-    else if (/android/i.test(userAgent)) {
-        window.location.href = "https://play.google.com/store/apps/details?id=com.begers.fitnessapp"; // Android için yönlendirme
-    }
-    // Mobil cihaz değilse
-    else {
-        window.location.href = "#download"; // Mobil cihaz değilse indirme bölümüne yönlendirme
-    }
+        // iPhone kontrolü
+        if (/iPhone/i.test(userAgent)) {
+            window.location.href = "https://apps.apple.com/app/id6479473338"; // iPhone için yönlendirme
+        }
+        // Android kontrolü
+        else if (/android/i.test(userAgent)) {
+            window.location.href = "https://play.google.com/store/apps/details?id=com.begers.fitnessapp"; // Android için yönlendirme
+        }
+        // Mobil cihaz değilse
+        else {
+            window.location.href = "#download"; // Mobil cihaz değilse indirme bölümüne yönlendirme
+        }
+    });
 });
