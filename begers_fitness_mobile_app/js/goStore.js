5a853edbfba51dc868f6fbff4508c297d83bcc58
BekirGeris <48486272+BekirGeris@users.noreply.github.com>
2024-12-03 00:14:38 +0300

    fix

---
+++ begers_fitness_mobile_app/js/goStore.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    // Butona tıklama olayı
+    $('#MyButton').click(function(event){
+      event.preventDefault(); // Varsayılan davranışı engelle
+
+      var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+      // iPhone kontrolü
+      if (/iPhone/i.test(userAgent)) {
+        window.location.href = "https://apps.apple.com/app/id6479473338"; // iPhone için yönlendirme
+      }
+      // Android kontrolü
+      else if (/android/i.test(userAgent)) {
+        window.location.href = "https://play.google.com/store/apps/details?id=com.begers.fitnessapp"; // Android için yönlendirme
+      }
+      // Mobil cihaz değilse
+      else {
+        window.location.href = "#download"; // Mobil cihaz değilse indirme bölümüne yönlendirme
+      }
+    });
+  });
